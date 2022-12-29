6f77fbc54e0b6da4a0ff11f6501f3bb93269f186
Syed Shayan Sohail <72223862+shayan-sohail@users.noreply.github.com>
2022-12-29 16:08:25 +0500

    Update server.cpp

--- network/app/server.cpp
+++ network/app/server.cpp
@@ -3,14 +3,14 @@
 int main()
 {
     std::array<char, 50> buffer;
-    Socket socket(SocketType::UDP, "127.0.0.1", 8890), receiverInfo;
+    Socket socket(SocketType::UDP, "127.0.0.1", 8890);
     socket.bind_socket();
     std::cout << socket.getErrorString() << std::endl;
     printf("Last Error: %d\n", WSAGetLastError());
     while (1)
     {
         std::cout << "Receiving data...\n";
-        int nBytes = socket.receive(receiverInfo, buffer.data(), 10);
+        int nBytes = socket.receive(buffer.data(), 10);
         if (nBytes > 0) {
             buffer[nBytes] = '\0';
             printf("Message Received %s\n", buffer.data());
